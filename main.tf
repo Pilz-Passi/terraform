2120975457e03ba70eee71a5e70749977f9516a3
Pascal S <131711689+Pilz-Passi@users.noreply.github.com>
2023-07-05 15:55:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,6 @@ resource "aws_instance" "deham6demo"{
     vpc_security_group_ids = ["sg-0e45a52719eb759fd"]
     subnet_id = "subnet-0bb7d846cd02da113"
     tags = {
-        Name = "deham6demos"
+        Name = "deham6demo"
     }
 }
