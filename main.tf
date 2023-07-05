3b816417e9b37a4acec07f61d7aeb3986fc3da94
Pascal S <131711689+Pilz-Passi@users.noreply.github.com>
2023-07-05 16:11:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,10 @@
 provider "aws" {
   region = "us-west-2"
 }
-resource "aws_s3_bucket" "dev" {
-  bucket = "uniquebucketnameforpuroposeof"
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
 
 resource "aws_instance" "deham6demo"{
     ami = "ami-0fa167c2af1ea0840"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     key_name = "vockey"
     vpc_security_group_ids = ["sg-0e45a52719eb759fd"]
     subnet_id = "subnet-0bb7d846cd02da113"
