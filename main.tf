4aa39ba89100504f6cca4a393cd4365a55544800
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-13 10:22:56 +0200

    add aws instance deham6demo 2nd time

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ resource "aws_instance" "deham6demo"{
     ami = "ami-0fa167c2af1ea0840"
     instance_type = "t3.micro"
     key_name = "vockey"
-    vpc_security_group_ids = ["sg-0e45a52719eb759fd"]
-    subnet_id = "subnet-0bb7d846cd02da113"
+    vpc_security_group_ids = ["sg-0acaa46ca7912ea65"]
+    subnet_id = "subnet-0cd64936af6fd342a"
     tags = {
         Name = "deham6demo"
     }
