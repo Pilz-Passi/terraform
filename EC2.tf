76188cf6fdec392ec70ceb260fa40f8adcd4f4ab
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-13 13:03:14 +0200

    changed EC2 subnetID

--- EC2.tf
+++ EC2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "deham6demo1"{
     ami = "ami-0ae49954dfb447966"
     instance_type = "t3.micro"
     key_name = "vockey"
-    vpc_security_group_ids = ["sg-09865e0155ddfa334"]
+    vpc_security_group_ids = ["sg-01012e9941338c293"]
     subnet_id = "subnet-0a799df905c521d71"
     tags = {
         Name = "deham6demo1"
