93eb70fff0c82b21d5b70681fea70242a45dc668
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-13 14:13:14 +0200

    changed subnet vars

--- EC2.tf
+++ EC2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "deham6demo1"{
     ami = "ami-0ae49954dfb447966"
     instance_type = "t3.micro"
     key_name = "vockey"
-    vpc_security_group_id = aws_security_group.id
+    vpc_security_group_ids = [aws_security_group.devVPC_sg_allow_ssh_http.id]
     subnet_id = aws_subnet.devVPC_public_subnet.id
     tags = {
         Name = "deham6demo1"
