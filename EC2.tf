26c8f3415943a63764a4dcbf4fcf6adec3ff3a23
Pascal Spielvogel <pascal-spielvogel@gmx.de>
2023-07-13 14:07:33 +0200

    subsidaried hard coded IDs with vars

--- EC2.tf
+++ EC2.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "deham6demo1"{
     ami = "ami-0ae49954dfb447966"
     instance_type = "t3.micro"
     key_name = "vockey"
-    vpc_security_group_ids = ["sg-01012e9941338c293"]
-    subnet_id = "subnet-0a799df905c521d71"
+    vpc_security_group_id = aws_security_group.id
+    subnet_id = aws_subnet.devVPC_public_subnet.id
     tags = {
         Name = "deham6demo1"
     }
